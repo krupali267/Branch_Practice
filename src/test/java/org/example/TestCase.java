8e1a3ad5765978c87b441c2a6cf2cb38a46a8e88
krupali267 <krupali26.goradia@gmail.com>
2021-05-15 16:25:25 +0100

    second commit

---
+++ src/test/java/org/example/TestCase.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.Test;
+
+public class TestCase {
+
+    @Test
+    public void itShouldAddTwoNumbers(){
+
+        System.out.println("Addition feature added");
+    }
+}
